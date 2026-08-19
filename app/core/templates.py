22fb3717d41516d49d111d6a5c0f9dbf63b121d7
Rakib Hossain <r.rakib.7254@gmail.com>
2026-08-19 23:49:58 +0600

    Fix undefined csrf_token in templates and remove deprecated docker-compose version key

--- app/core/templates.py
+++ app/core/templates.py
@@ -1,4 +1,5 @@
 import os
+import secrets
 from jinja2 import Environment, FileSystemLoader
 
 class DynamicTemplates:
@@ -12,6 +13,13 @@ class DynamicTemplates:
         # Fresh Jinja Environment per render to avoid cached global state issues
         env = Environment(loader=FileSystemLoader(self.directory), autoescape=True)
 
+        def csrf_token():
+            token = request.session.get("_csrf_token")
+            if not token:
+                token = secrets.token_hex(32)
+                request.session["_csrf_token"] = token
+            return token
+
         def get_flashed_messages(with_categories: bool = False, category_filter: list = ()):
             flash = request.session.pop("_flash", None)
             if not flash:
@@ -24,6 +32,7 @@ class DynamicTemplates:
                 return [(cat, msg)]
             return [msg]
 
+        env.globals["csrf_token"] = csrf_token
         env.globals["get_flashed_messages"] = get_flashed_messages
 
         render_context = {"request": request, **context}
